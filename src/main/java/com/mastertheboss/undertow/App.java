a7500663766a9becd40b8fddc78bb2b50215ee4a
Ian Henderson <ianwhen@gmail.com>
2016-02-12 10:04:34 -0800

    Update port handling

--- src/main/java/com/mastertheboss/undertow/App.java
+++ src/main/java/com/mastertheboss/undertow/App.java
@@ -21,9 +21,20 @@ import io.undertow.server.*;
 import io.undertow.util.Headers;
 
 public class App {
+  private static boolean isEmpty(String s) {
+    return (s == null || s.isEmpty());
+  }
+
   public static void main(final String[] args) {
     String ENV_PORT = System.getenv("PORT");
-    int port = (ENV_PORT != null) ? Integer.parseInt(ENV_PORT) : 8080 ;
+    String SYS_PORT = System.getProperty("server.port");
+    System.out.println("ENV_PORT: " + ENV_PORT);
+    System.out.println("SYS_PORT: " + SYS_PORT);
+    int port = (!isEmpty(ENV_PORT)) ?
+      Integer.parseInt(ENV_PORT) :
+      (!isEmpty(SYS_PORT)) ?
+        Integer.parseInt(SYS_PORT) :
+        8080 ;
     System.out.println("Listening on port: " + port);
     Undertow server = Undertow.builder().addHttpListener(port, "localhost")
       .setHandler(new HttpHandler() {
