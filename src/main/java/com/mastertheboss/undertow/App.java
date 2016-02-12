cacb9cd07f1b1b296ddc0b4da6e8c3ff44e084be
Ian Henderson <ianwhen@gmail.com>
2016-02-12 00:38:41 -0800

    Update port setting

--- src/main/java/com/mastertheboss/undertow/App.java
+++ src/main/java/com/mastertheboss/undertow/App.java
@@ -1,10 +1,10 @@
 // package com.mastertheboss.undertow;
-// 
+//
 // /**
 //  * Hello world!
 //  *
 //  */
-// public class App 
+// public class App
 // {
 //     public static void main( String[] args )
 //     {
@@ -22,7 +22,10 @@ import io.undertow.util.Headers;
 
 public class App {
   public static void main(final String[] args) {
-    Undertow server = Undertow.builder().addHttpListener(8080, "localhost")
+    String ENV_PORT = System.getenv("PORT");
+    int port = (ENV_PORT != null) ? Integer.parseInt(ENV_PORT) : 8080 ;
+    System.out.println("Listening on port: " + port);
+    Undertow server = Undertow.builder().addHttpListener(port, "localhost")
       .setHandler(new HttpHandler() {
         public void handleRequest(final HttpServerExchange exchange) throws Exception {
           exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
